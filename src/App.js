de654afe73d5a55f10e360623ed22d93f256c1c4
Ekeu Ulrich <ekeuulrich2@gmail.com>
2020-04-16 00:57:41 +0200

    Setting up routes using react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <Navbar/>
-    <Landing/>
-  </Fragment>
+  <Router>
+    <Navbar />
+    <Route exact path='/' component={Landing} />
+    <section className='container'>
+      <Switch>
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Switch>
+    </section>
+  </Router>
 );
 
 export default App;

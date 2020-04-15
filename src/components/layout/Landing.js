4719e63005b9e2627e8accdc72f77bff00ded4b1
Ekeu Ulrich <ekeuulrich2@gmail.com>
2020-04-16 00:38:33 +0200

    Project cleanup and components initialization

---
+++ src/components/layout/Landing.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Streaming Test</h1>
+          <p className='lead'>
+            I really wish to thank you all guys for this technical test. It
+            really helped me to improve and give the best out of me.
+          </p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Sign Up
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
